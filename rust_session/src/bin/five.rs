3bddd22d3198bc0a2d8337ec4646c4ef54549b6b
Alexandre <alexandre.regnier59@live.fr>
2026-01-02 15:50:40 +0100

    refactor(five): exclusion de la fonction 'guess_num' en dehors du main pour une meilleure lisibilité et capacité de test

--- rust_session/src/bin/five.rs
+++ rust_session/src/bin/five.rs
@@ -1,49 +1,44 @@
 use rand::Rng;
-use std::{io};
+use std::io;
+
 fn main() {
     println!("### Juste Prix ###");
 
-    //Générer un nombre avec rand
-    //thread_rng() : initialise le générateur
-    //gen_range(1..=100) : génère un nombre entre 1 et 100 INCLUS !!
-    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
-    
-    
-
-    fn guess_num() -> i32 {
-        let mut guess: String= String::new();
-        loop{
-            guess.clear();
-            println!("Entrez un nombre");
-            io::stdin()
-                .read_line(&mut guess)
-                .expect("Erreur lecture");
-
-            match guess.trim().parse() {
-                Ok(num) => {return num;
-                },
-                Err(_) => {
-                    println!("Entrez un nombre valide !");
-                    continue;
-            }
-        };
-    }       
-}
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
         let nb_guessed = guess_num();
 
         if nb_guessed == secret_number {
-            println!("Juste prix ! Vous avez gagné rien du tout !");
+            println!("Juste prix ! Vous avez gagné !");
             break;
         } else if nb_guessed > secret_number {
             println!("C'est moins !");
         } else {
             println!("C'est plus !");
-        };
-
-         
-            }
         }
+    }
+}
 
-        
+// Fonctions définies hors du main, en bas !
+
+fn guess_num() -> i32 {
+    let mut guess = String::new();
+    
+    loop {
+        guess.clear();
+        println!("Entrez un nombre :");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Erreur lecture");
+
+        match guess.trim().parse() {
+            Ok(num) => return num, 
+            Err(_) => {
+                println!("Ceci n'est pas un nombre valide !");
+                continue;
+            }
+        };
+    }
+}
