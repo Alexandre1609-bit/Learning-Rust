c5d7baf2cc762151496a6dc861b69d12bb85ee7f
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-02-21 22:18:36 +0100

    feat: add match in order to prevent updating while server being online.

--- rust_session/src/bin/server_t.rs
+++ rust_session/src/bin/server_t.rs
@@ -2,8 +2,27 @@ fn main() {
     let mut srv = Server::new("Server one".to_string());
 
     srv.get_status();
-    srv.change_status(ServerStatus::Online);
+    srv.change_status(ServerStatus::Updating);
+    match srv.change_status(ServerStatus::Online) {
+        Ok(s) => println!("Server status : {}", s),
+        Err(msg) => println!("Error"),
+    }
     srv.get_status();
+
+    match srv.change_status(ServerStatus::Updating) {
+        Ok(s) => println!("Server status : {}", s),
+        Err(msg) => println!("Error : {}", msg),
+    }
+
+    match srv.change_status(ServerStatus::Offline) {
+        Ok(s) => println!("Server status : {}", s),
+        Err(msg) => println!("Error : {}", msg),
+    }
+
+    match srv.change_status(ServerStatus::Updating) {
+        Ok(s) => println!("Server status : {}", s),
+        Err(msg) => println!("Erros : {}", msg),
+    }
 }
 
 enum ServerStatus {
@@ -31,7 +50,7 @@ impl Server {
                 println!("### Server is ready ###")
             }
             ServerStatus::Offline => {
-                println!("### Server is shutdown ###")
+                println!("### Server is offline ###")
             }
             ServerStatus::Updating => {
                 println!("### Server is currently updating ###")
@@ -39,7 +58,24 @@ impl Server {
         }
     }
 
-    fn change_status(&mut self, new_status: ServerStatus) {
-        self.status = new_status;
+    fn change_status(&mut self, new_status: ServerStatus) -> Result<String, String> {
+        match new_status {
+            ServerStatus::Online => {
+                self.status = new_status;
+                Ok("Online mode set correctly".to_string())
+            }
+            ServerStatus::Offline => {
+                self.status = new_status;
+                Ok("Server is now offline".to_string())
+            }
+            ServerStatus::Updating => match &self.status {
+                ServerStatus::Updating => Ok("Server is updating".to_string()),
+                ServerStatus::Online => Err("Can not update while online".to_string()),
+                ServerStatus::Offline => {
+                    self.status = new_status;
+                    Ok("Server is now updating".to_string())
+                }
+            },
+        }
     }
 }
