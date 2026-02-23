cb1c30a9a3fa48f30c6e5c3e6a0e6f2960ffcfd2
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-02-23 21:45:01 +0100

    feat: add router in loop, add Box & dyn

--- rust_session/src/bin/nexus_infra/main.rs
+++ rust_session/src/bin/nexus_infra/main.rs
@@ -5,25 +5,30 @@ use router::Router;
 use server::Server;
 
 fn main() {
-    let mut cluster: Vec<Server> = Vec::new();
+    let mut cluster: Vec<Box<dyn Pingable>> = Vec::new();
+
+    let router = Box::new(Router::new("rt_box".to_string(), "10.0.0.2".to_string(), 8));
+
+    cluster.push(router);
 
     for i in 1..=10 {
         let srv_name = format!("srv{}", i,);
-
         let srv_ip = format!("192.168.0.{}", i);
+        cluster.push(Box::new(Server::new(srv_name, srv_ip))); //Technique à retenir !   
 
-        cluster.push(Server::new(srv_name, srv_ip)); //Technique à retenir !
+        let rt_name = format!("rt{}", i);
+        let rt_ip = format!("10.0.0.{}", i);
+        cluster.push(Box::new(Router::new(rt_name, rt_ip, 4)));
     }
 
     for s in &cluster {
-        start_diag(s);
+        start_diag(s.as_ref());
     }
 }
-
 pub trait Pingable {
     fn ping(&self);
 }
 
-fn start_diag(equipment: &impl Pingable) {
+fn start_diag(equipment: &dyn Pingable) {
     equipment.ping();
 }
