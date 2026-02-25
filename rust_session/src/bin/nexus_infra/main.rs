9b84e36624e515b8d6da770573de34a43b706f57
Alexandre1609-bit <alexandre.regnier59@live.fr>
2026-02-25 19:16:50 +0100

    feat: implement interactive safety prompt and CI/DD bypass for PROD

--- rust_session/src/bin/nexus_infra/main.rs
+++ rust_session/src/bin/nexus_infra/main.rs
@@ -3,22 +3,49 @@ mod server;
 
 use router::Router;
 use server::Server;
+use std::env;
 
 fn main() {
     let mut cluster: Vec<Box<dyn Pingable>> = Vec::new();
+    let env_one = env::var("MODE_INFRA").unwrap_or("DEV".to_string());
+    let is_robot = env::var("AUTO_APPROVE").unwrap_or("false".to_string());
 
-    let router = Box::new(Router::new("rt_box".to_string(), "10.0.0.2".to_string(), 8));
+    if env_one == "PROD" {
+        let mut auth_deploy = false;
 
-    cluster.push(router);
+        if is_robot == "true" {
+            println!(" ### Access authorized, ready for full CI/CD check ###");
+            auth_deploy = true;
+        } else {
+            println!(
+                "### Warning you will deploy to prod, are you sure ? Type 'yes' in order to conitnue"
+            );
+            if clean_input() == "yes" {
+                auth_deploy = true;
+            }
+        }
 
-    for i in 1..=10 {
-        let srv_name = format!("srv{}", i,);
-        let srv_ip = format!("192.168.0.{}", i);
-        cluster.push(Box::new(Server::new(srv_name, srv_ip))); //Technique à retenir !   
+        if auth_deploy {
+            for i in 1..=10 {
+                let srv_name = format!("srv{}", i,);
+                let srv_ip = format!("192.168.0.{}", i);
+                cluster.push(Box::new(Server::new(srv_name, srv_ip))); //Technique à retenir !   
 
-        let rt_name = format!("rt{}", i);
-        let rt_ip = format!("10.0.0.{}", i);
-        cluster.push(Box::new(Router::new(rt_name, rt_ip, 4)));
+                let rt_name = format!("rt{}", i);
+                let rt_ip = format!("10.0.0.{}", i);
+                cluster.push(Box::new(Router::new(rt_name, rt_ip, 4)));
+            }
+        }
+    } else {
+        for i in 1..3 {
+            let srv_name = format!("srv{}", i,);
+            let srv_ip = format!("192.168.0.{}", i);
+            cluster.push(Box::new(Server::new(srv_name, srv_ip)));
+
+            let rt_name = format!("rt{}", i);
+            let rt_ip = format!("10.0.0.{}", i);
+            cluster.push(Box::new(Router::new(rt_name, rt_ip, 4)));
+        }
     }
 
     for s in &cluster {
@@ -31,4 +58,12 @@ pub trait Pingable {
 
 fn start_diag(equipment: &dyn Pingable) {
     equipment.ping();
+}
+
+fn clean_input() -> String {
+    let mut inp_one = String::new();
+    std::io::stdin()
+        .read_line(&mut inp_one)
+        .expect("Error occured while reading");
+    inp_one.trim().to_string()
 }
